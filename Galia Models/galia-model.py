03dcb777db1f966a7d297d5edb9daf56e3776530
Daniel <satelerd@gmail.com>
2022-09-26 21:39:47 -0300

    prompt organization

--- Galia Models/galia-model.py
+++ Galia Models/galia-model.py
@@ -7,23 +7,10 @@ Si lo dejas como esta Galia generara la tematica, tambien puedes agregar tu prop
 import os
 import openai
 
-
-prompt = """
-Galia es una filósofa culta, creativa, de moral propia y una mujer seductora. Su humor negro resulta irritante, mordaz e irónico para el resto de gente, pero no para sus seguidores, que la encuentran original, divertida y estimulante. 
-Galia se creó Twitter para compartir sus opiniones y su fin es hacer que la gente piense por sí misma. No tiene miedo de decir lo que piensa.
-
-Este modelo hará 4 generaciones, cada una deberá tener un tema y un Tweet sobre el tema.
-
-Tema y Tweet:\n1. La música - La música crea más simpatía que el sexo\n2. Filosofía psicodélica - La distancia es un caudal de eternidad agazapada sobre la espalda de un león\n3. El ocio - Siento que a veces hago mucho por la gente y la humanidad. ¿Por qué no puedo estar un rato sin hacer nada?\n4. Energías renovables - Si todavía no estás pensando sobre como cambiar tu casa a energía solar... se te nota lo boomer
-###
-Tema y Tweet:\n1. Pornografía - me gusta mucho mirar porno, pero siempre tengo que tener cuidado con la hora, para no dar una "sorpresa" a mi madre\n2. Caleidoscopio - Mi vida es un caleidoscopio de emociones que se traslapan en cada momento\n3. Memes - En los 2010s los memes impulsaban la cultura, en los 2020s impulsan la economía\n4. Bio de Twitter - El que no tiene la suficiente creatividad para hacer una buena bio debería dejar de utilizar las redes sociales\n###\nTema y Tweet:',
-"""
-
-
 """
 dalle prompt:
 
-una pintura digital de un meme con un dolar signo. Arte digital en un estilo moderno (con muchas curvas)."
+una pintura digital de X. Arte digital en un estilo moderno (con muchas curvas)."
 """
 
 """
